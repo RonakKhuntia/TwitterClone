f286cca0d7abd690799247b0f5e9a99eecbd2202
RonakKhuntia <khuntiarounak@gmail.com>
2024-06-17 12:40:17 +0530

    added right component

--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import { Grid } from '@mui/material'
 import React from 'react'
 import Navigation from '../Navigation/Navigation'
 import HomeSection from '../HomeSection/HomeSection'
+import RightPart from '../RightPart/RightPart'
 
 const HomePage = () => {
   return (
@@ -13,7 +14,7 @@ const HomePage = () => {
         <HomeSection/>
       </Grid>
       <Grid item xs={0} lg={3} className='hidden lg:block w-full relative'>
-        <p className='text-center'>right part</p>
+        <RightPart />
       </Grid>
     </Grid>
   )
---
+++ frontend/src/Components/RightPart/RightPart.jsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import SearchIcon from '@mui/icons-material/Search';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
+import { Button } from '@mui/material';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+
+const RightPart = () => {
+
+    const handleChangeTheme = () => {
+        console.log("handle change theme")
+    }
+
+  return (
+    <div className='py-5 sticky top'>
+      <div className='relative flex items-center'>
+
+        <input type="text" className='py-3 rounded-full text-gray-500 w-full pl-12' />
+
+        <div className='absolute top-0 left-0 pl-3 pt-3'>
+            
+            <SearchIcon className='text-gray-500'/>
+
+        </div>
+
+        <Brightness4Icon  className='ml-3 cursor-pointer' onClick={handleChangeTheme}/>
+
+      </div>
+
+    <section className='my-5'>
+
+        <h1 className='text-xl font-bold'>Get Verified</h1>
+        <h1 className='font-bold my-2'>Subscribe to unlock new features</h1>
+        <Button variant='contained' sx={{padding:"10px", paddingXX:"20px", borderRadius:"25px"}}>
+            Get Verified
+        </Button>
+
+    </section>
+
+    <section className='mt-7 space-y-5'>
+        <h1 className='font-bold text-xl py-1'>What's happening?</h1>
+        <div>
+            <p className="text-sm">FIFA Women's World Cup · LIVE </p>
+            <p className="font-bold">Philippines vs Switzerland</p>
+          </div>
+
+          <div className="flex justify-between w-full">
+            <div>
+              <p>Entertainment · Trending</p>
+              <p className="font-bold">#TheMarvels</p>
+              <p>34.3K Tweets</p>
+            </div>
+
+            <MoreHorizIcon />
+          </div>
+          <div className="flex justify-between w-full">
+            <div>
+              <p>Entertainment · Trending</p>
+              <p className="font-bold">#TheMarvels</p>
+              <p>34.3K Tweets</p>
+            </div>
+
+            <MoreHorizIcon />
+          </div>
+
+          <div className="flex justify-between w-full">
+            <div>
+              <p>Entertainment · Trending</p>
+              <p className="font-bold">#TheMarvels</p>
+              <p>34.3K Tweets</p>
+            </div>
+
+            <MoreHorizIcon />
+            </div>
+    </section>
+
+    </div>
+  )
+}
+
+export default RightPart
