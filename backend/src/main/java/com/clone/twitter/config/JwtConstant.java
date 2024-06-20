e39225edee415ffbc7e0d634074c898936a4891a
RonakKhuntia <khuntiarounak@gmail.com>
2024-06-20 16:50:14 +0530

    initialised security configuration

---
+++ backend/src/main/java/com/clone/twitter/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.clone.twitter.config;
+
+public class JwtConstant {
+
+    public static final String SECRET_KEY="sdjbfsdbfgljhsdvfgvsdgfvcsjkdvgfaksdgVGVSDFKGFgfvddsivfd";
+    public static final String JWT_HEADER="Authorization";
+}
