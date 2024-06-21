6b67dfacd274012d07dcf37e7db3bce28b32d07e
RonakKhuntia <khuntiarounak@gmail.com>
2024-06-21 12:31:54 +0530

    added jwt config

--- backend/src/main/java/com/clone/twitter/config/JwtTokenValidator.java
+++ backend/src/main/java/com/clone/twitter/config/JwtTokenValidator.java
@@ -37,7 +37,7 @@ public class JwtTokenValidator extends OncePerRequestFilter {
             }catch(Exception e){
                 throw new BadCredentialsException("invalid token");
             }
-            filterChain.doFilter(request,response);
         }
+        filterChain.doFilter(request,response);
     }
 }
