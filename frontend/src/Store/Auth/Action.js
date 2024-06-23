a55e5229bf60c65c6e677c1b75acd37735af81d9
RonakKhuntia <khuntiarounak@gmail.com>
2024-06-23 14:42:59 +0530

    added  reducer

---
+++ frontend/src/Store/Auth/Action.js
@@ -0,0 +1,44 @@
+import axios from "axios"
+import { API_BASE_URL } from "../../config/Api"
+import { GET_USER_PROFILE_FAILURE, GET_USER_PROFILE_SUCCESS, LOGIN_USER_FAILURE, LOGIN_USER_SUCCESS, REGISTER_USER_SUCCESS } from "./ActionType"
+
+
+export const loginUser = (loginData) => async(dispatch) => {
+    try{
+        const {data} = await axios.post(`${API_BASE_URL}/auth/signin`,loginData)
+        if(data.jwt){
+            localStorage.setItem("jwt",data.jwt)
+        }
+        dispatch({type:LOGIN_USER_SUCCESS,payload:data.jwt})
+    }catch(error){
+        console.log("error",error)
+        dispatch({type:LOGIN_USER_FAILURE,payload:error.message})
+    }
+}
+
+export const registerUser = (registerData) => async(dispatch) => {
+    try{
+        const {data} = await axios.post(`${API_BASE_URL}/auth/signup`,registerData)
+        if(data.jwt){
+            localStorage.setItem("jwt",data.jwt)
+        }
+        dispatch({type:REGISTER_USER_SUCCESS,payload:data.jwt})
+    }catch(error){
+        console.log("error",error)
+        dispatch({type:REGISTER_USER_FAILURE,payload:error.message})
+    }
+}
+
+export const getUserProfile = (jwt) => async(dispatch) => {
+    try{
+        const {data} = await axios.get(`${API_BASE_URL}/api/users/profile`,{
+            headers:{
+                "Authorization": `Bearer ${jwt}`
+            }
+        })
+        dispatch({type:GET_USER_PROFILE_SUCCESS,payload:data})
+    }catch(error){
+        console.log("error",error)
+        dispatch({type:GET_USER_PROFILE_FAILURE,payload:error.message})
+    }
+}
---
+++ frontend/src/Store/Store.js
@@ -0,0 +1,10 @@
+import { combineReducers, legacy_createStore, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
+import { authReducer } from "./Auth/Reducer";
+
+
+const rootReducers = combineReducers({
+    auth:authReducer,
+});
+
+export const store = legacy_createStore(rootReducers, applyMiddleware(thunk));
